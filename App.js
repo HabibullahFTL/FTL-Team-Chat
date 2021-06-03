55f9810b2feb1fabd5449fa7f558ca03b5476ccd
Habibullah Bahar Piash <habibullahftl@gmail.com>
2021-06-03 20:59:09 +0600

    start working in named repo

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>FTL Team Chat!</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ config.firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDGH_Csge0mnhfbTD2PJLfbfKFgoXZlrFk",
+    authDomain: "ftl-team-chat.firebaseapp.com",
+    projectId: "ftl-team-chat",
+    storageBucket: "ftl-team-chat.appspot.com",
+    messagingSenderId: "764705360736",
+    appId: "1:764705360736:web:9987435b4143c2d6136c05"
+};
