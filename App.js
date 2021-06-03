47468f7bfb8c89af0ccf5eb2d1770f9035e3abc8
Habibullah Bahar Piash <habibullahftl@gmail.com>
2021-06-03 22:17:22 +0600

    login page designed

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './Screens/LoginScreen';
+
+const Stack = createStackNavigator();
+const globalScreenOptions = {
+  headerStyle: { backgroundColor: "crimson" },
+  headerTitleStyle: { color: "white" , alignSelf: "center"},
+  headerTintColor: "white"
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>FTL Team Chat!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={globalScreenOptions}>
+        <Stack.Screen options={{
+          title: "Login Screen"
+        }} name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ Screens/LoginScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StatusBar } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
+
+const LoginScreen = () => {
+    return (
+        <View>
+            <StatusBar style="light"/>
+            <Text>FTL Team Chat!</Text>
+        </View>
+    )
+}
+
+export default LoginScreen
+
+const styles = StyleSheet.create({})
